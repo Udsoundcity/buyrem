898af1199225c3c30dae68079187ca15783a365c
Udsoundcity <Udsoundcity@gmail.com>
2026-06-11 21:32:59 +0100

    Meta isssues

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
-import script from "next/script";
+
+import FacebookPixel from "@/app/components/Facebookpixel";
 import "./globals.css";
 import ConditionalLayout from "./components/ConditionalLayout";
 import { STORE_NAME, STORE_TAGLINE } from "@/lib/constants";
@@ -24,50 +25,16 @@ export default function RootLayout({ children }) {
         />
 
       
- <Script
-          id="facebook-pixel"
-          strategy="afterInteractive"
-        >
-          {`
-            !function(f,b,e,v,n,t,s)
-            {
-              if(f.fbq)return;
-              n=f.fbq=function(){
-                n.callMethod ?
-                n.callMethod.apply(n,arguments) :
-                n.queue.push(arguments)
-              };
-              if(!f._fbq)f._fbq=n;
-              n.push=n;
-              n.loaded=!0;
-              n.version='2.0';
-              n.queue=[];
-              t=b.createElement(e);
-              t.async=!0;
-              t.src=v;
-              s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s);
-            }(
-              window,
-              document,
-              'script',
-              'https://connect.facebook.net/en_US/fbevents.js'
-            );
-
-            fbq('init', '1337290678502790');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+ 
 
 
       </head>
       <body>
+         <Facebookpixel />
         <SpeedInsights/>
         <ConditionalLayout>{children}</ConditionalLayout>
         <SpeedInsights />
-         <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1337290678502790&ev=PageView&noscript=1"
-/></noscript>
+  
       </body>
     </html>
   );
