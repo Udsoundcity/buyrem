4ac29f5e0982fe058c5aa32d427d7cc77529314d
Udsoundcity <Udsoundcity@gmail.com>
2026-06-11 21:58:58 +0100

    Go

--- app/layout.js
+++ app/layout.js
@@ -23,13 +23,14 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
-        
+
       </head>
       <body>
          <FacebookPixel />
         <SpeedInsights/>
         <ConditionalLayout>{children}</ConditionalLayout>
         <SpeedInsights />
+        <FacebookPixel />
   
       </body>
     </html>
