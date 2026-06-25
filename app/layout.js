8292cbd0a753e402378c8deb85d610b052392e61
Udsoundcity <Udsoundcity@gmail.com>
2026-06-25 09:19:49 +0100

    gyrhjksdjksj

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import ConditionalLayout from "./components/ConditionalLayout";
 import { STORE_NAME, STORE_TAGLINE } from "@/lib/constants";
 import { getSettings } from "@/lib/settings";
+import FacebookPixel from "./components/Facebookpixel";
 
 
 
@@ -36,11 +37,11 @@ export default async function RootLayout({ children }) {
       </head>
 
       <body>
-      
+       <FacebookPixel />
         <ConditionalLayout>{children}</ConditionalLayout> 
 
         {/* ── Meta Pixel — injected from admin settings ── */}
-        
+        <FacebookPixel />
       </body>
     </html>
   );
